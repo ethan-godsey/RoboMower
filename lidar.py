6154090ddadcb91c6338fbf5b67fa27ee677a868
Ethan Godsey <egodsey@iwu.edu>
2026-05-04 00:34:03 -0500

    reset issue fixed

--- lidar.py
+++ lidar.py
@@ -9,10 +9,11 @@ LIDAR_PORT = '/dev/ttyAMA0'
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LIDAR_MOTOR_PIN, GPIO.OUT)
 pwm = GPIO.PWM(LIDAR_MOTOR_PIN, 10000)
-pwm.start(100)
+pwm.start(60)
 time.sleep(4)
 
 lidar = RPLidar(LIDAR_PORT)
+lidar.reset()
 lidar.clean_input()
 
 try:
