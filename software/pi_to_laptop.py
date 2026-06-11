a42dadddcfd7f289d2b1d671bdeb1b143afd84e5
Ethan Godsey <egodsey73@gmail.com>
2026-06-10 21:11:11 -0500

    client server

--- software/pi_to_laptop.py
+++ software/pi_to_laptop.py
@@ -8,6 +8,7 @@ IP = "0.0.0.0"
 async def handler(websocket):
     # add connection from client
     CONN.add(websocket)
+    print("connection made!")
     try:
         # just sit here until client disconnects, and remove this connection from q
         await websocket.wait_closed()  
@@ -20,13 +21,14 @@ async def broadcast(queue):
         # create blocking call to get data from queue
         scan = await asyncio.get_event_loop().run_in_executor(None, queue.get)
         data = json.dumps(scan)
-
+        
         # send it out to listening clients
         websockets.broadcast(CONN, data)
 
 async def main(queue):
     # spinup server, run the handler, asynchronously sending the queue data once it comes in
     async with websockets.serve(handler, IP, 3000):
+        print("started!")
         await broadcast(queue)
 
 def main_wrap(queue):
