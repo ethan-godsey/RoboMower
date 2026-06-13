46491ebf04ab1dc0881474fd94235946f9859e15
Ethan Godsey <egodsey@iwu.edu>
2026-06-13 11:02:20 -0500

    app.vue

--- software/robo-frontend/src/App.vue
+++ software/robo-frontend/src/App.vue
@@ -1,19 +1,35 @@
 
 <template>
-  <canvas ref="lidarCanvas" width="600" height="600"></canvas>
+  <div class="info">
+    <canvas ref="lidarCanvas" width="600" height="600"></canvas>
+    <p>X: {{ state[0] }}, Y: {{ state[1] }}<b> {{ ((Math.PI / 180) * state[2]).toFixed(1) }}</b></p>
+  </div>
 </template>
+
+<style>
+.info {
+  border-radius: 5rem;
+  border-color: black;
+  margin: 3rem;
+}
+</style>
 
 <script>
 export default {
   data() {
-    return { scanPoints: [], ws: null }
+    return { scanPoints: [], 
+             ws: null,
+             state: [] }
   },
   mounted() {
   this.ctx = this.$refs.lidarCanvas.getContext('2d')
 
   this.ws = new WebSocket('ws://172.20.10.7:3000')
+  // when port recieves an event, parse data
   this.ws.onmessage = (event) => {
-    this.scanPoints = JSON.parse(event.data)
+    const data = JSON.parse(event.data)
+    this.scanPoints = data.scan
+    this.state = data.state
     }
   },
   watch: {
@@ -23,19 +39,40 @@ export default {
   },
   methods: {
     draw() {
-      // clear canvas, convert polar -> cartesian, plot points
+      // clear canvas
       const canvas = this.$refs.lidarCanvas
       this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      // plot origin start for robot in red
+      this.ctx.fillStyle = 'red'
+      this.ctx.fillRect(300, 300, 1, 1)
+      
+      // convert polar to cartesian
       this.ctx.fillStyle = 'lime'
       for (const meas of this.scanPoints) {
         const [quality, angle, distance] = meas
-        const angle_rad = angle * Math.PI / 180
+        const angle_rad = angle *  (180 / Math.PI)
         const x_mm = distance * Math.cos(angle_rad)
         const y_mm = distance * Math.sin(angle_rad)
-        const x_px = (x_mm + 6000) / 20
-        const y_px = (y_mm + 6000) / 20
+
+        // translate onto canvas grid of 600 x 600 and plot
+        const x_px = (x_mm + 3000) / 10
+        const y_px = (y_mm + 3000) / 10
         this.ctx.fillRect(x_px, y_px, 2, 2)
       }
+
+      const [x, y, heading] = this.state
+
+      // from the origin, measure the change to map relative point we are at (mm)
+      const dx = 0 - (x * 1000)
+      const dy = 0 - (y * 1000) 
+
+      // use change times heading difference to calculate coordinates
+      const local_x = ((dx * Math.cos(heading) + dy * Math.sin(heading)) + 3000) / 10
+      const local_y = (-dx * Math.sin(heading) + dy * Math.cos(heading) + 3000) / 10
+
+      this.ctx.fillStyle = 'black'
+      this.ctx.fillRect(local_x, local_y, 3, 3)
     }
   }
 }
